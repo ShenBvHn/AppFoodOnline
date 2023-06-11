640a5f054c1899857ee00ecc04eb07bbdf30fc79
son <nguyendacson1311@gmail.com>
2023-06-11 16:05:30 +0700

    [V]Apply new google service

--- app/src/main/java/com/example/appfoodmixi/MainActivity.java
+++ app/src/main/java/com/example/appfoodmixi/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.appfoodmixi;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
+import com.example.appfoodmixi.View.Account.SignInActivity;
+import com.example.appfoodmixi.View.HomeActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -23,44 +28,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
-        // Add a new document with a generated ID
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            // check ktra đã đăng nhập vào thẳng vào Home
+            public void run() {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    if (firebaseAuth.getCurrentUser().getEmail().length() > 0) {
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
                     }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
-                    }
-                });
+                } else {
+                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
+                }
 
-//        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-//
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            // check ktra đã đăng nhập vào thẳng vào Home
-//            public void run() {
-//                if (firebaseAuth.getCurrentUser() != null) {
-//                    if (firebaseAuth.getCurrentUser().getEmail().length() > 0) {
-//                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
-//                    }
-//                } else {
-//                    startActivity(new Intent(MainActivity.this, SignInActivity.class));
-//                }
-//
-//                finish();
-//            }
-//        }, 3500);
+                finish();
+            }
+        }, 3500);
     }
 }
